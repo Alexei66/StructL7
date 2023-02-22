579b9773a3b42a0e653c3c0d3c3919be3aa84dbc
Alexei66 <borisenkosenee@gmail.com>
2023-02-23 00:25:46 +0200

    Скопировал метод чтения файла, конструктор, поля и свойства

---
+++ Diary.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructL7
+{
+    internal class Diary
+    {
+        /// <summary>
+        /// Номер записи
+        /// </summary>
+        public int id;
+
+        /// <summary>
+        /// Время записи
+        /// </summary>
+        private DateTime currentDateTime;
+
+        /// <summary>
+        /// Заметка/Список дел/План
+        /// </summary>
+        private string toDoList;
+
+        /// <summary>
+        /// Имя авотра записи
+        /// </summary>
+        private string authorsName;
+
+        /// <summary>
+        /// Главный пункт для выолнения
+        /// </summary>
+        private string mainTask;
+
+        public int Id
+        { get { return this.id; } set { this.id = value; } }
+
+        public string ToDoList
+        { get { return this.toDoList; } set { this.toDoList = value; } }
+
+        public string AuthorsName
+        { get { return this.authorsName; } set { this.authorsName = value; } }
+
+        public string MainTask
+        { get { return this.mainTask; } set { this.mainTask = value; } }
+
+        public DateTime CurrentDateTime
+        { get { return this.currentDateTime; } set { this.currentDateTime = value; } }
+
+        /// <summary>
+        /// Конструктор создания записи в ежедневник
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="CurrentDateTime"></param>
+        /// <param name="ToDoList"></param>
+        /// <param name="MainTask"></param>
+        /// <param name="AuthorsName"></param>
+        public Diary(int Id, DateTime CurrentDateTime, string ToDoList, string MainTask, string AuthorsName)
+        {
+            this.id = Id;
+            this.currentDateTime = CurrentDateTime;
+            this.toDoList = ToDoList;
+            this.authorsName = AuthorsName;
+            this.mainTask = MainTask;
+        }
+
+        /// <summary>
+        /// Вывод на экран
+        /// </summary>
+        /// <returns></returns>
+        public string Print()
+        {
+            return $"{this.id,10} {this.currentDateTime,10} {this.toDoList,10} {this.mainTask,10}  {this.authorsName,10}";
+        }
+    }
+}
---
+++ Possibilities.cs
@@ -0,0 +1,65 @@
+﻿using Example_751;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructL7
+{
+    internal class Possibilities
+    {
+        private Diary[] diary;  // Основной массив для хранения данных
+
+        private string path; // путь к файлу с данными
+
+        private int index; // текущий элемент для добавления в workers
+
+        private string[] titles; // массив, храняий заголовки полей. используется в PrintDbToConsole
+
+        public Possibilities(string Path)
+        {
+            this.path = Path; // Сохранение пути к файлу с данными
+            this.index = 0; // текущая позиция для добавления сотрудника в workers
+            this.titles = new string[0]; // инициализаия массива заголовков
+            this.diary = new Diary[1]; // инициализаия массива сотрудников.    | изначально предпологаем, что данных нет
+
+            this.Load(); // Загрузка данных
+        }
+
+        private void Load()
+        {
+            using (StreamReader sr = new StreamReader(this.path))
+            {
+                titles = sr.ReadLine().Split(',');
+
+                while (!sr.EndOfStream)
+                {
+                    string[] args = sr.ReadLine().Split(',');
+
+                    AddRecord(new Diary(Convert.ToInt32(args[0]), DateTime.Parse(args[1]), args[2], args[3], args[4]));
+                }
+            }
+        }
+
+        public void AddRecord()
+        { }
+
+        private static void DeleteRecord()
+        { }
+
+        public void EditRecord()
+        { }
+
+        private static void Save()
+        { }
+
+        private void Resize(bool Flag)
+        {
+            if (Flag)
+            {
+                Array.Resize(ref this.diary, this.diary.Length * 2);
+            }
+        }
+    }
+}
